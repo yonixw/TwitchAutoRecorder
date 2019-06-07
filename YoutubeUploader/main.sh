98d4362ff12f72802033e56be6d405a9a3cbb04c
Ubuntu <ubuntu@ip-172-31-36-103.eu-west-1.compute.internal>
2019-06-07 04:05:26 +0000

    Uploader works too. Ready for production?

--- YoutubeUploader/main.sh
+++ YoutubeUploader/main.sh
@@ -1,1 +1,20 @@
-#
+#!/bin/bash
+
+while true
+    do
+
+    files="./allrecords/*/*.mp4"
+
+    for f in $files
+    do
+        name=$(basename $f)
+        echo [`date +%F-%T`] File $name
+        youtube-upload --client-secrets=/src/app/.client_secrets.json --title="$name" --privacy private $f
+        rm -f $f
+    done
+
+    echo [`date +%F-%T`] Done uploading, waiting 5m
+    sleep 5m
+done
+
+
