f14d21f9d974cbf9568b26736cfc22b9956b4767
Ubuntu <ubuntu@ip-172-31-36-103.eu-west-1.compute.internal>
2019-06-09 18:56:09 +0000

    Cool down between uploads... need to checl if cause for problemd

--- YoutubeUploader/main.sh
+++ YoutubeUploader/main.sh
@@ -7,10 +7,16 @@ while true
 
     for f in $files
     do
-        name=$(basename $f)
-        echo [`date +%F-%T`] File $name
-        youtube-upload --client-secrets=/src/app/.client_secrets.json --title="$name" --privacy private $f
-        rm -f $f
+        if [[ -f "$f" ]]; then
+            echo "$f exist"
+            name=$(basename $f)
+            echo [`date +%F-%T`] File $name
+            youtube-upload --client-secrets=/src/app/.client_secrets.json --title="$name" --privacy private $f
+            
+            echo [`date +%F-%T`] Done uploading $f, waiting 20m before next
+            sleep 20m
+            # rm -f $f
+        fi
     done
 
     echo [`date +%F-%T`] Done uploading, waiting 5m
